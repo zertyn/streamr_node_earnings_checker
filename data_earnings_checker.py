080549f60db083536d544b735a189a9049fff0c9
zertyn <38588045+zertyn@users.noreply.github.com>
2022-02-28 18:34:17 +0100

    Add files via upload

--- data_earnings_checker.py
+++ data_earnings_checker.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 
-from bs4 import BeautifulSoup as bs
 import requests as rq
 from datetime import datetime
-
+import json
+    
 def strfdelta(tdelta, fmt):
     d = {"days": tdelta.days}
     d["hours"], rem = divmod(tdelta.seconds, 3600)
     d["minutes"], d["seconds"] = divmod(rem, 60)
     return fmt.format(**d)
 
-# EDIT HERE variable addresses -> enter your public node addresses here
-addresses = ["","","","",""];
+# EDIT HERE variable addresses -> enter your public node addresses here (as many as you have, extend with comma)
+addresses = ["public_node_address_x","public_node_address_y","public_node_address_z"]
+
 # EDIT HERE variable datetime -> mining start date (year, month, day, hour, minute). Check first mining reward code in https://streamr-dashboard.vercel.app/node
 mining_start_datetime = datetime(2022, 2, 23, 0, 0)
 
-personalnodes_url = "https://streamr-dashboard.vercel.app/node?address="
-pricevalue_url = "https://coinmarketcap.com/currencies/streamr/"
-accumulateddata = 0;
+# for future additions (reward codes etc.)
+#personalnodes_url = "https://streamr-dashboard.vercel.app/node?address="
+
+# initalization of basic variables
+pricevalue_url = "https://min-api.cryptocompare.com/data/price?fsym=DATA&tsyms=USD"
+rewardendpoint_url = "https://brubeck1.streamr.network:3013/datarewards/"
+accumulated_data = 0;
+data_per_node = [];
 mining_time = datetime.now() - mining_start_datetime
 mining_time_formatted = strfdelta(mining_time, "{days} days {hours} hours {minutes} minutes")
 mining_days = mining_time.days
@@ -25,29 +31,41 @@ mining_hours, remainder = divmod(mining_time.seconds, 3600)
 
 # loops through addresses and fetches total DATA acquired per node
 for address in addresses:
-    0 == 0;
-    response = rq.get(personalnodes_url + address)
-    soup = bs(response.content, 'html.parser')
-    data_div = str(soup.find("div",attrs={"class","value svelte-f8rxn4"}))
-    accumulateddata += float(data_div[(data_div.index('">') + 2):(data_div.index('</') - 5)])
+    response = rq.get(rewardendpoint_url + address)
+    json_data = json.loads(response.text)
+    data_per_node.append(float(json_data["DATA"]))
+    accumulated_data += float(json_data["DATA"])
 
-# calculate revenue using Coinmarketcap current DATA worth
+# calculate revenue using Cryptocompare current DATA worth
 response = rq.get(pricevalue_url)
-soup = bs(response.content, 'html.parser')
-coinvalue_div = str(soup.find("div",attrs={"class","priceValue"}))
-coin_value = float(coinvalue_div[(coinvalue_div.index('<span>') + 7):(coinvalue_div.index('</span>') - 1)])
-rev_total = round(coin_value * accumulateddata, 2)
+json_data = json.loads(response.text)
+coin_value = float(json_data["USD"])
+
+rev_total = round(coin_value * accumulated_data, 2)
 rev_day = round(rev_total / mining_days, 2)
 rev_hour = round(rev_total / (mining_days * 24 + mining_hours), 2)
+est_rev_month = round(rev_total / (mining_days * 24 + mining_hours) * 720, 2)
+est_rev_year = round(rev_total / (mining_days * 24 + mining_hours) * 8760, 2)
 
 # prints all variables into a nice overview
-print('\n############## STREAMR NODE EARNINGS ##############')
-print(f'\n   Total time mined: {mining_time_formatted}')
-print(f'\n          Accumulated coins: {round(accumulateddata, 2)} DATA')
-print(f'               Total revenue: ${rev_total}')
-print(f'          Average revenue per day: ${rev_day}')
-print(f'          Average revenue per hour: ${rev_hour}')
-print(f'\n##############  DATA VALUE: ${coin_value}  ##############')
+print('\n################ STREAMR NODE EARNINGS ################')
+print(f'\n     Total time mined: {mining_time_formatted}')
+print(f'                    Total nodes: {len(addresses)}')
+print('\n_____________________ REVENUE _________________________')
+print(f'\n            Accumulated coins: {round(accumulated_data, 2)} DATA')
+print(f'                 Total revenue: ${rev_total}')
+print(f'            Average revenue per day: ${rev_day}')
+print(f'            Average revenue per hour: ${rev_hour}')
+print('\n____________________ NODE STATS _______________________\n')
+
+for index, address in enumerate(addresses):
+    print(f'                 Node {index + 1}: {data_per_node[index]} DATA')
+
+print('\n____________________ ESTIMATES ________________________\n')
+print(f'   Estimated monthly revenue: ${est_rev_month} or { round(est_rev_month / coin_value, 2)} DATA')
+print(f'  Estimated yearly revenue: ${est_rev_year} or { round(est_rev_year / coin_value ,2)} DATA')
+
+print(f'\n################  DATA VALUE: ${coin_value}  ################')
 
 input("Press Enter to continue...")
 
